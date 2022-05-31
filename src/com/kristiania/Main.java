98a5945c1b8535e09a8499cd63da4684405e8e2c
August Elvevold <elvevold@yahoo.no>
2022-06-01 01:19:12 +0200

    Finally finished

--- src/com/kristiania/Main.java
+++ src/com/kristiania/Main.java
@@ -1,10 +1,9 @@
 package com.kristiania;
 
-import javax.security.auth.login.CredentialException;
-
 public class Main {
     public static void main(String[] args) {
 
+        // Fills database with all questions and answers
         new CreateDatabase();
 
         QuizGame newGame = new QuizGame();
