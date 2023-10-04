4aaa6bf6183acd8ae8ce52d728ea572a06f6c4e6
AugustElvevold <89490288+AugustElvevold@users.noreply.github.com>
2023-10-04 16:25:40 +0200

    Update Create user and database.sql

--- Create user and database.sql
+++ Create user and database.sql
@@ -1,3 +1,7 @@
+-- If new installation of MySQL and not workbench, run these commands first in order to test java project:
+-- mysql -u root
+-- mysql -u root -p (if already user)
+
 CREATE USER 'User1'@'localhost' IDENTIFIED by '123456';
 
 CREATE DATABASE IF NOT EXISTS `quizdb`;
@@ -5,3 +9,5 @@ CREATE DATABASE IF NOT EXISTS `quizdb`;
 USE `quizdb`;
 
 GRANT ALL ON `quizdb` TO `User1`;
+
+-- exit;
