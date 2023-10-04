0b3f45c25aa53a900555c15917d032c2cbfb264a
AugustElvevold <elvevold@yahoo.no>
2023-10-04 09:27:38 +0200

    Add note to run "Create user and database.sql" file first in Main

--- src/com/kristiania/Main.java
+++ src/com/kristiania/Main.java
@@ -3,6 +3,9 @@ package com.kristiania;
 public class Main {
     public static void main(String[] args) {
 
+        // NOTE
+        // RUN "Create user and database.sql" BEFORE RUNNIN MAIN
+
         // Fills database with all questions and answers
         new CreateDatabase();
 
